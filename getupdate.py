c6e9cf5976f78626b11985a1c45f8201c9ec007b
dohcgle <utn1002@gmail.com>
2021-01-31 16:51:56 +0500

    first

---
+++ getupdate.py
@@ -0,0 +1,10 @@
+#  @tutorial_ulugbek_bot
+
+import requests
+from pprint import pprint
+token = '1610753174:AAHpqhQf1kdJ1AFOKdZFS14gleR76XwzSHQ'
+url = f'https://api.telegram.org/bot{token}/getUpdates'
+r = requests.get(url)
+data = r.json()
+updates = data['result']
+pprint(updates['text'])
