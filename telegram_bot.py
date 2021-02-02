e40ccfad755daa3055c468a26dbcdc4410d97ee9
ulugbek-ubuntu-20 <utn1002@gmail.com>
2021-02-02 13:37:50 +0500

    check send message

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,8 +3,13 @@
 import requests
 from pprint import pprint
 token = '1610753174:AAHpqhQf1kdJ1AFOKdZFS14gleR76XwzSHQ'
-# url = f'https://api.telegram.org/bot{token}/getMe'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
-r= requests.get(url)
+r = requests.get(url)
 user = r.json()
+
+sendurl = f'https://api.telegram.org/bot{token}/sendMessage'
+
+params = {'chat_id': 476916789, 'text': 'test'}
+requests.get(url=sendurl, params=params)
+
 pprint(user)
